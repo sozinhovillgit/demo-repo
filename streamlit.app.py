7cb1993ba63c8d33239d6435cfa90fe983da87d1
sozinhovillgit <116233291+sozinhovillgit@users.noreply.github.com>
2023-09-25 08:50:28 +0900

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,1 +1,11 @@
-s
+import streamlit as st
+import pandas as pd
+import plost
+
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+with open('style.css') as f: 
+  st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+st.sidebar.header('Dashboar `version 2`')
+
